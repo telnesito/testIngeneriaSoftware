39ba79383fb7d3d0ae6a5930d2a520e8f2dbfeb0
Carlos <carlosj_1703@hotmail.com>
2023-04-15 22:08:36 -0400

    addModule

--- src/components/EditorRuby.jsx
+++ src/components/EditorRuby.jsx
@@ -1,28 +1,27 @@
-import React, { useRef, useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './EditorRuby.css'
-import editorSettings from '../functions/editorSettings'
-import rubyWorker from 'monaco-editor/esm/vs/basic-languages/ruby/ruby?worker';
 import saveLocalStorage from '../functions/saveLocalStorage';
+import Editor from '@monaco-editor/react';
 
 const EditorRuby = () => {
   //Variable que guarda lo que mostrara en pantalla
   let salida = ''
   //Referencia del DOM
-  const [savedItem,] = useState(window.localStorage.getItem('code-ruby'))
-  const [codigo, setCodigo] = useState(savedItem)
+  const { localStorage } = window
+  const [savedItem, setSavedItem] = useState(localStorage.getItem('ruby-code'))
+  const [savedItem2, setSavedItem2] = useState(localStorage.getItem('ruby-code-dos'))
+  const initialView = 'main'
+  const [viewState, setViewState] = useState(initialView)
+
+
+
+  let codigo = savedItem || ''
+  let codigoDos = savedItem2 || ''
   const refOutput = useRef()
-  const refCode = useRef()
+
 
   const [autoCompile, setAutoCompile] = useState('true')
 
-  window.MonacoEnvironment = {
-    getWorker(workerId, label) {
-      if (label === 'ruby') {
-        return new rubyWorker
-      }
-
-    }
-  }
 
   //Funcion que remplaza el console.log para mostrar los parametros por pantalla 
   const puts = (...argumentos) => {
@@ -39,9 +38,14 @@ const EditorRuby = () => {
       salida = ''
       console.log = puts
 
+
       //Compilamos y ejecutmos el codigo Ruby con Opal
 
+      let libreria = Opal.compile(codigoDos)
       let output = Opal.compile(codigo)
+
+
+      eval(libreria)
       eval(output)
 
       //Mostrar salida en pantalla
@@ -49,40 +53,117 @@ const EditorRuby = () => {
         refOutput.current.innerHTML = salida
       }
 
-
     } catch (e) {
       refOutput.current.innerHTML = e.message
-      console.warn(e)
+
 
     }
   }
 
-  const validateAutoCompile = () => {
-    if (autoCompile === 'true') {
-      handleUpdate()
-    } else {
-      return
+
+
+  const Prob = () => {
+    const validateAutoCompile = (valor, event) => {
+
+      codigo = valor
+
+
+      if (autoCompile === 'true') {
+        handleUpdate()
+      } else {
+        return
+      }
+    }
+
+    const validateAutoCompile2 = (value, event) => {
+
+      codigoDos = value
+
+      if (autoCompile === 'true') {
+        handleUpdate()
+      } else {
+        return
+      }
+    }
+    if (viewState === 'main') {
+      return (
+        <>
+          <Editor
+            theme={'vs-dark'}
+            language={'ruby'}
+            onChange={validateAutoCompile}
+            loading={<h1>Cargando main</h1>}
+            value={savedItem}
+
+          />
+          <br></br>
+
+          <button className='btn-save' onClick={() => handleSave('ruby-code', codigo)}>Guardar en localStorage</button>
+        </>
+
+      )
+    }
+
+    if (viewState === 'library') {
+      return (
+        <>
+          <Editor
+            theme={'vs-dark'}
+            language={'ruby'}
+            onChange={validateAutoCompile2}
+            loading={<h1>Cargando librerias</h1>}
+            value={savedItem2}
+          />
+          <br></br>
+
+          <button className='btn-save' onClick={() => handleSave('ruby-code-dos', codigoDos)}>Guardar en localStorage</button>
+        </>
+
+      )
+    }
+
+    if (viewState === 'utils') {
+      return <h1>Nada</h1>
     }
   }
 
-  useEffect(() => {
-    editorSettings(savedItem, 'ruby', 'vs-dark', refCode.current, setCodigo)
-  }, []);
-
-  const handleSave = () => {
-    saveLocalStorage('code-ruby', codigo)
+  const handleSave = (Editor, actualCodigo = '') => {
+    saveLocalStorage(Editor, actualCodigo)
   }
+
+  const handleChangeView = (view, loadCode, localCode) => {
+    setViewState(view)
+    loadCode(localStorage.getItem(localCode))
+  }
+
 
   return (
     <div>
-      <h2>Compilador Ruby</h2>
+      <h2>INTERPRETADOR :P Ruby</h2>
+
+      <div className='menu-archivos'>
+        <button onClick={() => handleChangeView('main', setSavedItem, 'ruby-code')} >main.rb</button>
+        <button onClick={() => handleChangeView('library', setSavedItem2, 'ruby-code-dos')}>funciones.rb</button>
+        <button onClick={() => handleChangeView('utils')}>utilidades.rb</button>
+      </div>
+      <br />
+
+      <h2>Si te mueves entre archivos sin guardar los cambios, tu codigo se perdera</h2>
+
+      <br />
+
       <div className='contenedor-ruby'>
-        <label >
-          <div ref={refCode} onKeyUp={() => validateAutoCompile()} id='editor-ruby'></div>
-        </label>
+
+        <div id='editor-ruby' >
+          <Prob />
+        </div>
+
 
         <div ref={refOutput} id='output-ruby'></div>
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
 
       <div className='btn'>
         <input defaultChecked onChange={({ target }) => setAutoCompile(target.value)} type={'radio'} name={'auto-compile-ruby'} id={'ruby-true'} value={'true'}></input>
@@ -98,7 +179,7 @@ const EditorRuby = () => {
         <button className='btn-compilar' onClick={handleUpdate} >Compilar</button>
         <br />
         <br />
-        <button className='btn-save' onClick={handleSave}>Guardar en localStorage</button>
+
       </div>
 
 
